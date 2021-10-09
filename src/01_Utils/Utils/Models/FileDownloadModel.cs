37f89f7cc5f0573f6ac91371c32d655024fccdb2
OldLi <wenju1991@163.com>
2021-10-09 18:54:50 +0800

    新增通用文件下载模型以及通用文件下载方法

---
+++ src/01_Utils/Utils/Models/FileDownloadModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Mkh.Utils.Models
+{
+    /// <summary>
+    /// 文件下载模型
+    /// </summary>
+    public class FileDownloadModel
+    {
+        /// <summary>
+        /// 文件完整路径
+        /// </summary>
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// 文件名
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// 文件内容类型
+        /// </summary>
+        public string ContentType { get; set; }
+    }
+}
--- src/03_Module/Module.Web/ControllerAbstract.cs
+++ src/03_Module/Module.Web/ControllerAbstract.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Mkh.Utils.Models;
 
 namespace Mkh.Module.Web
 {
@@ -12,6 +13,14 @@ namespace Mkh.Module.Web
     [ValidateResultFormat]
     public abstract class ControllerAbstract : ControllerBase
     {
-        
+        /// <summary>
+        /// 文件下载
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        protected IActionResult FileDownload(FileDownloadModel model)
+        {
+            return PhysicalFile(model.FilePath, model.ContentType ?? "application/octet-stream", model.FileName, true);
+        }
     }
 }
