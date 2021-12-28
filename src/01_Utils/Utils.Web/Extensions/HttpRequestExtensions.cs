3c7841538bdc3131d857b7862d010425926cb906
OLDLI <wenju1991@163.com>
2021-12-28 16:11:39 +0800

    新增HttpRequestExtensions扩展类

---
+++ src/01_Utils/Utils.Web/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Mkh.Utils.Web.Extensions
+{
+    public static class HttpRequestExtensions
+    {
+        /// <summary>
+        /// 获取指定路径的完整URL
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="path">子路径</param>
+        /// <returns></returns>
+        public static string GetUrl(this HttpRequest request, string path = null)
+        {
+            return $"{request.Scheme}://{request.Host}{request.PathBase}{path ?? string.Empty}";
+        }
+    }
+}
