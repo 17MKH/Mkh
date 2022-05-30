681a071f0992043bd69912858d69d9ae22ac882a
OLDLI <wenju1991@163.com>
2022-05-30 23:26:29 +0800

    update

--- modules/00_Admin/Admin.UI/build/app.config.js
+++ modules/00_Admin/Admin.UI/build/app.config.js
@@ -6,9 +6,7 @@ import useBaseConfig from './base.config'
 export default defineConfig(({ mode, command }) => {
   const config = useBaseConfig({ target: 'app', mode, command })
 
-  if (mode === 'development') {
-    config.resolve.alias['vue-i18n'] = 'vue-i18n/dist/vue-i18n.cjs.js'
-  } else if (mode == 'production') {
+  if (mode == 'production') {
     config.build = {
       outDir: '../../WebHost/wwwroot/web',
     }
