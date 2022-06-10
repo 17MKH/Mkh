77a271a3a6b628782fdfd410c2663365f5178b7d
OLDLI <wenju1991@163.com>
2022-06-10 09:10:06 +0800

    update

---
+++ src/07_Config/Config.Core/ModuleConfigureContextExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Mkh.Config.Abstractions;
+using Mkh.Module.Abstractions;
+
+namespace Mkh.Config.Core
+{
+    public static class ModuleConfigureContextExtensions
+    {
+        /// <summary>
+        /// 获取配置实例
+        /// </summary>
+        /// <typeparam name="TConfig"></typeparam>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static TConfig GetConfig<TConfig>(this ModuleConfigureContext context) where TConfig : IConfig, new()
+        {
+            return context.Services.BuildServiceProvider().GetService<IConfigProvider>()!.Get<TConfig>();
+        }
+    }
+}
