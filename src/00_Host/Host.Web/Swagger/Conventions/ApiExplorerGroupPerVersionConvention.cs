7ffaa46709759998ad0ccaead968507ff218828b
mschen_ <czhcom@163.com>
2022-09-02 11:20:10 +0800

    Update ApiExplorerGroupPerVersionConvention.cs
    自定义命名空间前缀导致Swagger分组冲突问题，异常：No operations defined in spec

--- src/00_Host/Host.Web/Swagger/Conventions/ApiExplorerGroupPerVersionConvention.cs
+++ src/00_Host/Host.Web/Swagger/Conventions/ApiExplorerGroupPerVersionConvention.cs
@@ -12,7 +12,14 @@ public class ApiExplorerGroupConvention : IControllerModelConvention
         if (controller.ControllerType.Namespace.IsNull())
             return;
 
-        string[] array = controller.ControllerType.FullName.Split('.');
-        controller.ApiExplorer.GroupName = array[2].ToLower();
+        /*
+         按照命名规范，假如前缀定义 xx.xx, 如：xx.xx.Mod.{模块名}.Web 时，按array[2]索引取分组的方式，
+         将导致分组冲突（No operations defined in spec）
+         */
+        var fname = controller.ControllerType.FullName;
+        var reg = Regex.Match(fname, "[.]+Mod[.]");
+        fname = reg.Success ? fname.Substring(reg.Index + reg.Length) : fname;
+        string[] array = fname.Split('.');
+        controller.ApiExplorer.GroupName = array.Length > 0 ? array[0].ToLower() : fname;
     }
 }
