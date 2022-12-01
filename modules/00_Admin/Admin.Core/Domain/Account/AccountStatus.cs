ef495c7514bf7d5c3709d3600dd12ea1032e65eb
OLDLI <wenju1991@163.com>
2022-12-01 14:11:18 +0800

    publish 1.3.12

---
+++ modules/00_Admin/Admin.Core/Domain/Account/AccountStatus.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace Mkh.Mod.Admin.Core.Domain.Account;
+
+/// <summary>
+/// 账户状态
+/// </summary>
+public enum AccountStatus
+{
+    /// <summary>
+    /// 未知
+    /// </summary>
+    [Description("未知")]
+    UnKnown = -1,
+    /// <summary>
+    /// 注册
+    /// </summary>
+    [Description("注册")]
+    Register,
+    /// <summary>
+    /// 激活
+    /// </summary>
+    [Description("激活")]
+    Active,
+    /// <summary>
+    /// 禁用
+    /// </summary>
+    [Description("禁用")]
+    Disabled
+}
